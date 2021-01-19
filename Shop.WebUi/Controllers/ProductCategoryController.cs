7330e809a803a4d2518e9391ee60d642033bc46f
PereiraMV <32296556+PereiraMV@users.noreply.github.com>
2021-01-19 12:17:47 +0100

    Category controller and views OK.

---
+++ Shop.WebUi/Controllers/ProductCategoryController.cs
@@ -0,0 +1,175 @@
+﻿using Shop.Core.Models;
+using Shop.DataAcess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.WebUi.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        ProductCategoryRepository context;
+
+        public ProductCategoryController()
+        {
+            context = new ProductCategoryRepository();
+        }
+
+
+
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<ProductCategory> products = context.Collection().ToList();
+            return View(products);
+        }
+
+
+        public ActionResult Create()
+        {
+            ProductCategory p = new ProductCategory();
+            return View(p);
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Create(ProductCategory p)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(p);
+            }
+            else
+            {
+                context.Insert(p);
+                context.saveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                ProductCategory p = context.FindById(id);
+                if (p == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View(p);
+                }
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+
+            }
+
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Edit(ProductCategory product, int id)
+        {
+
+            try
+            {
+                ProductCategory prodTotEdit = context.FindById(id);
+                if (prodTotEdit == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        return View(product);
+                    }
+                    else
+                    {
+                        //context.Update(product); ce n'est pas un context Entity Framework
+
+                        prodTotEdit.Category = product.Category;
+                        prodTotEdit.Id = product.Id;
+
+
+
+
+                        context.saveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+
+
+
+            }
+            catch (Exception)
+            {
+
+                return HttpNotFound();
+            }
+
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                ProductCategory p = context.FindById(id);
+
+                if (p == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View(p);
+                }
+            }
+            catch (Exception)
+            {
+
+                return HttpNotFound();
+            }
+
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+
+            try
+            {
+                ProductCategory p = context.FindById(id);
+
+                if (p == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    context.Delete(id);
+                    context.saveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+
+                return HttpNotFound();
+            }
+
+        }
+
+
+    }
+}
